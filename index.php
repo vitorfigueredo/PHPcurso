05109116be327fc23348b5b99ccf50da5f28b949
Vitor Figueredo <vitor.s.figueredo@gmail.com>
2019-01-19 21:02:49 -0200

    Aprendendo sobre Operadores parte 2

--- index.php
+++ index.php
@@ -47,3 +47,42 @@ function variavel_global_acessivel(){
 }
 
 variavel_global_acessivel();
+
+echo "</br>";
+echo "</br>";
+
+$valor_um = 10;
+$valor_dois = 10;
+
+// Operadores Atribuição
+
+$op_atribuicao = "=";
+$op_concatenacao = " . " . "<= é o ponto mesmo";
+$op_adicao_de_variavel = '$variavel_1 .= "o que adicionar (normalmente texto)"';
+$op_incrementar_variavel_valor_qualquer = '$variavel += valor(numero)';
+$op_decrementar_variavel_valor_qualquer = '$variavel -= valor(numero)';
+$op_multiplicar_variavel_valor_qualquer = '$variavel *= valor(numero)';
+$op_dividir_variavel_valor_qualquer = '$variavel /= valor(numero)';
+$op_potencializar_variavel_valor_qualquer = '$variavel **= valor(numero)';
+
+echo $valor_um +=2; // testar operadores de Atribuição
+echo "</br>";
+echo "</br>";
+
+//Operadores Aritiméticos (operações matemáticas)
+$op_adicao = "+";
+$op_subtracao = "-";
+$op_multiplicacao = "*";
+$op_divisao = "/";
+$op_potencia = "**";
+$op_resto_divisao = "%";
+
+echo $valor_dois % 3; // testar operadores de Aritiméticos
+
+echo "</br>";
+echo "</br>";
+
+//Operadores de comparação
+$op_igualdade = "==";
+$op_maior = ">";
+$op_menor = "<";
