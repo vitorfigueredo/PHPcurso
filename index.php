767887002522cabb89e8e35b2bf599984086239b
Vitor Figueredo <vitor.s.figueredo@gmail.com>
2019-01-19 19:34:38 -0200

    Incluindo Algumas variaéveis  Globais

--- index.php
+++ index.php
@@ -12,6 +12,16 @@ $data = new DateTime(); // Novo objeto
 
 var_dump($carros); // mostra informação sobre a variável em tela
 
+echo "</br>";
+echo "</br>";
+
 $testeGet = $_GET["a"];
 
 var_dump($testeGet);
+
+echo "</br>";
+echo "</br>";
+
+$ip_usuario = $_SERVER["REMOTE_ADDR"];
+
+var_dump($ip_usuario);
