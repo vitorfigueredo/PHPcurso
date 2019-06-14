88d9518fd17f5e9dc15b59f4136412fd80ca5bfc
vitorfigueredo <vitor.s.figueredo@gmail.com>
2019-06-14 13:11:42 -0300

    Primera classe criada

--- objetos/index.php
+++ objetos/index.php
@@ -1,1 +1,21 @@
 <?php
+
+echo "Orientação a Objetos";
+
+class Primeira {
+    //Acesso liberado a todos que fazem chamada da classe
+    public $nome; // Atributo
+    
+    public function falar(){ // Método
+        
+        return "Meu nome é " . $this->nome;//$this é variável para chamar os atributos e incorporar cada classe
+
+    }
+}
+
+$vitor = new Primeira();
+$vitor->nome = "Vitor Figueredo";
+
+echo "</br>";
+
+echo $vitor->falar();
